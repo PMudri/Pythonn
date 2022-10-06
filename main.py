62a9083466b071309e91a06c54c677ec71ec879c
PMudri <115134447+PMudri@users.noreply.github.com>
2022-10-06 07:45:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+echo "# Pythonn" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PMudri/Pythonn.git
+git push -u origin main
